bca2ae2ed5314707dcc641956b6fb2d79a5fdd21
Anton Rusev <anton.rusev@limechain.tech>
2024-05-31 10:24:01 +0300

    feat: use native function to resolve network gateways, instead of env var
    Signed-off-by: Anton Rusev <anton.rusev@limechain.tech>

--- src/chain/index.ts
+++ src/chain/index.ts
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import { assertNotNull } from '@subsquid/substrate-processor'
 import { ProcessorConfig, ChainApi, IChainData } from './interfaces'
+import { lookupArchive } from '@subsquid/archive-registry';
 
 const ensureEnvVariable = (name: string): string => {
   return assertNotNull(
@@ -12,11 +13,10 @@ const ensureEnvVariable = (name: string): string => {
 function getChain(): { api: ChainApi; config: ProcessorConfig } {
   const chainName = ensureEnvVariable('CHAIN')
   const chainRpcEndpoint = ensureEnvVariable('CHAIN_RPC_ENDPOINT')
-  const chainGateway = ensureEnvVariable('CHAIN_GATEWAY')
-
   const chainNameKebab = chainName.split('_').join('-')
+  console.log(`Using chain: ${chainName} (${chainNameKebab}) ${chainRpcEndpoint}`)
   const chainAPI = require(`./${chainNameKebab}`).default
-
+  
   let chainsConfig: IChainData[]
   try {
     const data = fs.readFileSync('assets/chains-data.json')
@@ -34,7 +34,9 @@ function getChain(): { api: ChainApi; config: ProcessorConfig } {
   let processorConfig: ProcessorConfig = {
     name: chainConfig.network,
     prefix: chainConfig.prefix,
-    gateway: chainGateway,
+    gateway: lookupArchive("kusama", {
+      release: "ArrowSquid",
+    }),
     endpoint: {
       url: chainRpcEndpoint,
       rateLimit: 10,
