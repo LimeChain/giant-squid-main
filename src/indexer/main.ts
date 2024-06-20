a8dfc501b8c665a1e7d8aab0803871bd02c1688a
Daniel Radoslavov <daniel.radoslavov@limechain.tech>
2024-06-20 16:25:11 +0300

    remove createIndexer function

--- src/indexer/main.ts
+++ src/indexer/main.ts
@@ -9,35 +9,6 @@ export function setupPallet<T>(setup: T): T {
   return setup;
 }
 
-export async function createIndexer({ config, pallets }: IndexerParams) {
-  const mapper = new PalletMapper(pallets, { chain: config.chain });
-
-  const events = mapper.getEvents();
-  const calls = mapper.getCalls();
-
-  const processor = new Processor(new TypeormDatabaseWithCache({ supportHotBlocks: true }), config);
-
-  processor.addEvents(events);
-  processor.addCalls(calls);
-  processor.start(async (ctx: ProcessorContext) => {
-    const queue: Action[] = [];
-
-    processItem(ctx.blocks, (block, item) => {
-      if (item.kind === 'event') {
-        const pallet = mapper.getEventPallet(item.value.name)!;
-        pallet.handle({ ctx, queue, block, item: item.value });
-      }
-      if (item.kind === 'call') {
-        const pallet = mapper.getCallPallet(item.value.name)!;
-        pallet.handle({ ctx, queue, block, item: item.value });
-      }
-    });
-
-    await Action.process(ctx, queue);
-    await ctx.store.flush();
-  });
-}
-
 export class Indexer {
   private mapper: PalletMapper;
   private processor: Processor;
