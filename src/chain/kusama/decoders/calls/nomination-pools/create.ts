bf12c7f7d517a2cec8c8d89bb809e8dfaf3663f6
Denis Dinkov <denisdinkov7@gmail.com>
2025-05-09 14:54:58 +0300

    Decoding improvements

--- src/chain/kusama/decoders/calls/nomination-pools/create.ts
+++ src/chain/kusama/decoders/calls/nomination-pools/create.ts
@@ -3,22 +3,21 @@ import { calls, v9291, v9420 } from '@/chain/kusama/types';
 import { Call, INominationPoolsCreatePollCallPalletDecoder } from '@/indexer';
 import { DataNotDecodableError } from '@/utils';
 
+const decodeMultiAddress = (address: v9291.MultiAddress | v9420.MultiAddress) => {
+  switch (address.__kind) {
+    case 'Address20':
+    case 'Address32':
+    case 'Id':
+    case 'Raw':
+      return address.value;
+
+    default:
+      throw new Error(`Unknown address type: ${address.__kind}`);
+  }
+};
 export class CreatePoolCallPalletDecoder implements INominationPoolsCreatePollCallPalletDecoder {
   decode(call: Call) {
     const { create } = calls.nominationPools;
-
-    const decodeMultiAddress = (address: v9291.MultiAddress | v9420.MultiAddress) => {
-      switch (address.__kind) {
-        case 'Address20':
-        case 'Address32':
-        case 'Id':
-        case 'Raw':
-          return address.value;
-
-        default:
-          throw new DataNotDecodableError(create, address);
-      }
-    };
 
     if (create.v9220.is(call)) {
       const { amount, root, nominator, stateToggler: toggler } = create.v9220.decode(call);
