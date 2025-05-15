374c6dcc81589946197fcea08cc211b96c09a063
nikolay_cholakov <nikolaicholakov25@gmail.com>
2025-05-15 12:41:49 +0300

    feat: index litentry

---
+++ src/chain/litentry/decoders/events/xTokens/transferredMultiAssets.ts
@@ -0,0 +1,208 @@
+import { events } from '@/chain/litentry/types';
+import { Event, ITransferredAssetsEventPalletDecoder } from '@/indexer';
+import assert from 'assert';
+import { UnknownVersionError } from '@/utils';
+
+import { V1Junction_Parachain, V1MultiAsset, V1MultiLocation } from '@/chain/litentry/types/v9101';
+import { V1MultiAsset as V1MultiAssetV2040, V1MultiLocation as V1MultiLocationV2040 } from '@/chain/litentry/types/v9150';
+import { V3MultiAsset, V3MultiLocation } from '@/chain/litentry/types/v9168';
+
+export class TransferredMultiAssetsEventPalletDecoder implements ITransferredAssetsEventPalletDecoder {
+  decode(event: Event) {
+    assert(event.call);
+    const transferredMultiAssets = events.xTokens.transferredMultiAssets;
+
+    if (transferredMultiAssets.v9101.is(event)) {
+      const { assets, dest, sender } = transferredMultiAssets.v9101.decode(event);
+      return {
+        from: sender,
+        to: getTo(dest),
+        toChain: getDestination(dest),
+        amount: getAssetAmounts(assets),
+        assets: getAssetIds(assets),
+      };
+    } else if (transferredMultiAssets.v9150.is(event)) {
+      const { assets, dest, sender } = transferredMultiAssets.v9150.decode(event);
+      return {
+        from: sender,
+        to: getTo(dest),
+        toChain: getDestination(dest),
+        amount: getAssetAmounts(assets),
+        assets: getAssetIds(assets),
+      };
+    } else if (transferredMultiAssets.v9168.is(event)) {
+      const { assets, dest, sender } = transferredMultiAssets.v9168.decode(event);
+      return {
+        from: sender,
+        to: getToV3(dest),
+        toChain: getDestination(dest),
+        amount: getAssetAmounts(assets),
+        assets: getAssetIdsV3(assets),
+      };
+    }
+
+    throw new UnknownVersionError(transferredMultiAssets);
+  }
+}
+
+function getDestination(destination: V1MultiLocation | V1MultiLocationV2040 | V3MultiLocation) {
+  if (destination.interior.__kind === 'X1') {
+    return 'Here';
+  }
+
+  if (destination.interior.__kind === 'X2' || destination.interior.__kind === 'X3') {
+    const target = destination.interior.value.find((e) => e.__kind === 'Parachain') as V1Junction_Parachain;
+
+    return target?.value?.toString();
+  }
+
+  return;
+}
+
+function getTo(destination: V1MultiLocation | V1MultiLocationV2040) {
+  if (destination.interior.__kind === 'X1') {
+    const target = destination.interior.value;
+    switch (target?.__kind) {
+      case 'AccountId32':
+        return target.id;
+      case 'AccountKey20':
+        return target.key;
+    }
+  }
+
+  if (destination.interior.__kind === 'X2' || destination.interior.__kind === 'X3') {
+    const target = destination.interior.value.find((e) => e.__kind === 'AccountId32' || e.__kind === 'AccountKey20' || e.__kind === 'GeneralKey');
+    switch (target?.__kind) {
+      case 'AccountId32':
+        return target.id;
+      case 'AccountKey20':
+        return target.key;
+      case 'GeneralKey':
+        return target.value;
+    }
+  }
+
+  return;
+}
+
+function getToV3(destination: V3MultiLocation) {
+  if (destination.interior.__kind === 'X1') {
+    const target = destination.interior.value;
+    switch (target?.__kind) {
+      case 'AccountId32':
+        return target.id;
+      case 'AccountKey20':
+        return target.key;
+    }
+  }
+
+  if (destination.interior.__kind === 'X2' || destination.interior.__kind === 'X3') {
+    const target = destination.interior.value.find((e) => e.__kind === 'AccountId32' || e.__kind === 'AccountKey20');
+    switch (target?.__kind) {
+      case 'AccountId32':
+        return target.id;
+      case 'AccountKey20':
+        return target.key;
+    }
+  }
+
+  return;
+}
+
+function getAssetAmounts(assets: (V1MultiAsset | V3MultiAsset | V1MultiAssetV2040)[]) {
+  return assets.map((asset) => (asset.fun.__kind === 'Fungible' ? asset.fun.value.toString() : undefined));
+}
+
+const DEFAULT_ASSET_ID = '0x0000';
+function getAssetIds(assets: V1MultiAssetV2040[]) {
+  return assets.map((asset) => {
+    switch (asset.id.__kind) {
+      case 'Concrete':
+        switch (asset.id.value.interior.__kind) {
+          case 'Here':
+            return [asset.id.value.interior.__kind, DEFAULT_ASSET_ID];
+          case 'X1':
+            return asset.id.value.interior.value.__kind === 'Parachain'
+              ? [asset.id.value.interior.value.value.toString(), DEFAULT_ASSET_ID]
+              : [undefined, undefined];
+          case 'X2': {
+            const assetChainId = asset.id.value.interior.value[0].__kind === 'Parachain' ? asset.id.value.interior.value[0].value.toString() : undefined;
+            let assetId = DEFAULT_ASSET_ID;
+
+            switch (asset.id.value.interior.value[1].__kind) {
+              case 'GeneralKey':
+                assetId = asset.id.value.interior.value[1].value;
+                break;
+            }
+
+            return [assetChainId, assetId];
+          }
+
+          case 'X3':
+          case 'X4':
+          case 'X5': {
+            const assetChainId = asset.id.value.interior.value[0].__kind === 'Parachain' ? asset.id.value.interior.value[0].value.toString() : undefined;
+            let assetId = DEFAULT_ASSET_ID;
+
+            switch (asset.id.value.interior.value[2].__kind) {
+              case 'GeneralIndex':
+                assetId = asset.id.value.interior.value[2].value.toString();
+                break;
+            }
+            return [assetChainId, assetId];
+          }
+
+          default:
+            throw new UnknownVersionError(asset.id.value.interior.__kind);
+        }
+      default:
+        throw new UnknownVersionError(asset.id.__kind);
+    }
+  });
+}
+
+function getAssetIdsV3(assets: V3MultiAsset[]) {
+  return assets.map((asset) => {
+    switch (asset.id.__kind) {
+      case 'Concrete':
+        switch (asset.id.value.interior.__kind) {
+          case 'Here':
+            return [asset.id.value.interior.__kind, DEFAULT_ASSET_ID];
+          case 'X1':
+            return asset.id.value.interior.value.__kind === 'Parachain'
+              ? [asset.id.value.interior.value.value.toString(), DEFAULT_ASSET_ID]
+              : [undefined, undefined];
+          case 'X2': {
+            const assetChainId = asset.id.value.interior.value[0].__kind === 'Parachain' ? asset.id.value.interior.value[0].value.toString() : undefined;
+            let assetId = DEFAULT_ASSET_ID;
+
+            switch (asset.id.value.interior.value[1].__kind) {
+              case 'GeneralKey':
+                assetId = asset.id.value.interior.value[1].data;
+                break;
+            }
+
+            return [assetChainId, assetId];
+          }
+
+          case 'X3':
+          case 'X4':
+          case 'X5': {
+            const assetChainId = asset.id.value.interior.value[0].__kind === 'Parachain' ? asset.id.value.interior.value[0].value.toString() : undefined;
+            let assetId = DEFAULT_ASSET_ID;
+
+            switch (asset.id.value.interior.value[2].__kind) {
+              case 'GeneralIndex':
+                assetId = asset.id.value.interior.value[2].value.toString();
+                break;
+            }
+            return [assetChainId, assetId];
+          }
+          default:
+            throw new UnknownVersionError(asset.id.value.interior.__kind);
+        }
+      default:
+        throw new UnknownVersionError(asset.id.__kind);
+    }
+  });
+}
--- src/chain/litentry/types/v9150.ts
+++ src/chain/litentry/types/v9150.ts
@@ -1,5 +1,300 @@
 import {sts, Result, Option, Bytes, BitSequence} from './support'
 
+export const V1MultiAsset: sts.Type<V1MultiAsset> = sts.struct(() => {
+    return  {
+        id: V1AssetId,
+        fun: V1Fungibility,
+    }
+})
+
+export const V1Fungibility: sts.Type<V1Fungibility> = sts.closedEnum(() => {
+    return  {
+        Fungible: sts.bigint(),
+        NonFungible: V1AssetInstance,
+    }
+})
+
+export const V1AssetInstance: sts.Type<V1AssetInstance> = sts.closedEnum(() => {
+    return  {
+        Array16: sts.bytes(),
+        Array32: sts.bytes(),
+        Array4: sts.bytes(),
+        Array8: sts.bytes(),
+        Blob: sts.bytes(),
+        Index: sts.bigint(),
+        Undefined: sts.unit(),
+    }
+})
+
+export type V1AssetInstance = V1AssetInstance_Array16 | V1AssetInstance_Array32 | V1AssetInstance_Array4 | V1AssetInstance_Array8 | V1AssetInstance_Blob | V1AssetInstance_Index | V1AssetInstance_Undefined
+
+export interface V1AssetInstance_Array16 {
+    __kind: 'Array16'
+    value: Bytes
+}
+
+export interface V1AssetInstance_Array32 {
+    __kind: 'Array32'
+    value: Bytes
+}
+
+export interface V1AssetInstance_Array4 {
+    __kind: 'Array4'
+    value: Bytes
+}
+
+export interface V1AssetInstance_Array8 {
+    __kind: 'Array8'
+    value: Bytes
+}
+
+export interface V1AssetInstance_Blob {
+    __kind: 'Blob'
+    value: Bytes
+}
+
+export interface V1AssetInstance_Index {
+    __kind: 'Index'
+    value: bigint
+}
+
+export interface V1AssetInstance_Undefined {
+    __kind: 'Undefined'
+}
+
+export type V1Fungibility = V1Fungibility_Fungible | V1Fungibility_NonFungible
+
+export interface V1Fungibility_Fungible {
+    __kind: 'Fungible'
+    value: bigint
+}
+
+export interface V1Fungibility_NonFungible {
+    __kind: 'NonFungible'
+    value: V1AssetInstance
+}
+
+export const V1AssetId: sts.Type<V1AssetId> = sts.closedEnum(() => {
+    return  {
+        Abstract: sts.bytes(),
+        Concrete: V1MultiLocation,
+    }
+})
+
+export type V1AssetId = V1AssetId_Abstract | V1AssetId_Concrete
+
+export interface V1AssetId_Abstract {
+    __kind: 'Abstract'
+    value: Bytes
+}
+
+export interface V1AssetId_Concrete {
+    __kind: 'Concrete'
+    value: V1MultiLocation
+}
+
+export interface V1MultiLocation {
+    parents: number
+    interior: V1Junctions
+}
+
+export type V1Junctions = V1Junctions_Here | V1Junctions_X1 | V1Junctions_X2 | V1Junctions_X3 | V1Junctions_X4 | V1Junctions_X5 | V1Junctions_X6 | V1Junctions_X7 | V1Junctions_X8
+
+export interface V1Junctions_Here {
+    __kind: 'Here'
+}
+
+export interface V1Junctions_X1 {
+    __kind: 'X1'
+    value: V1Junction
+}
+
+export interface V1Junctions_X2 {
+    __kind: 'X2'
+    value: [V1Junction, V1Junction]
+}
+
+export interface V1Junctions_X3 {
+    __kind: 'X3'
+    value: [V1Junction, V1Junction, V1Junction]
+}
+
+export interface V1Junctions_X4 {
+    __kind: 'X4'
+    value: [V1Junction, V1Junction, V1Junction, V1Junction]
+}
+
+export interface V1Junctions_X5 {
+    __kind: 'X5'
+    value: [V1Junction, V1Junction, V1Junction, V1Junction, V1Junction]
+}
+
+export interface V1Junctions_X6 {
+    __kind: 'X6'
+    value: [V1Junction, V1Junction, V1Junction, V1Junction, V1Junction, V1Junction]
+}
+
+export interface V1Junctions_X7 {
+    __kind: 'X7'
+    value: [V1Junction, V1Junction, V1Junction, V1Junction, V1Junction, V1Junction, V1Junction]
+}
+
+export interface V1Junctions_X8 {
+    __kind: 'X8'
+    value: [V1Junction, V1Junction, V1Junction, V1Junction, V1Junction, V1Junction, V1Junction, V1Junction]
+}
+
+export type V1Junction = V1Junction_AccountId32 | V1Junction_AccountIndex64 | V1Junction_AccountKey20 | V1Junction_GeneralIndex | V1Junction_GeneralKey | V1Junction_OnlyChild | V1Junction_PalletInstance | V1Junction_Parachain | V1Junction_Plurality
+
+export interface V1Junction_AccountId32 {
+    __kind: 'AccountId32'
+    network: V0NetworkId
+    id: Bytes
+}
+
+export interface V1Junction_AccountIndex64 {
+    __kind: 'AccountIndex64'
+    network: V0NetworkId
+    index: bigint
+}
+
+export interface V1Junction_AccountKey20 {
+    __kind: 'AccountKey20'
+    network: V0NetworkId
+    key: Bytes
+}
+
+export interface V1Junction_GeneralIndex {
+    __kind: 'GeneralIndex'
+    value: bigint
+}
+
+export interface V1Junction_GeneralKey {
+    __kind: 'GeneralKey'
+    value: WeakBoundedVec
+}
+
+export interface V1Junction_OnlyChild {
+    __kind: 'OnlyChild'
+}
+
+export interface V1Junction_PalletInstance {
+    __kind: 'PalletInstance'
+    value: number
+}
+
+export interface V1Junction_Parachain {
+    __kind: 'Parachain'
+    value: number
+}
+
+export interface V1Junction_Plurality {
+    __kind: 'Plurality'
+    id: V0BodyId
+    part: V0BodyPart
+}
+
+export type V0BodyPart = V0BodyPart_AtLeastProportion | V0BodyPart_Fraction | V0BodyPart_Members | V0BodyPart_MoreThanProportion | V0BodyPart_Voice
+
+export interface V0BodyPart_AtLeastProportion {
+    __kind: 'AtLeastProportion'
+    nom: number
+    denom: number
+}
+
+export interface V0BodyPart_Fraction {
+    __kind: 'Fraction'
+    nom: number
+    denom: number
+}
+
+export interface V0BodyPart_Members {
+    __kind: 'Members'
+    count: number
+}
+
+export interface V0BodyPart_MoreThanProportion {
+    __kind: 'MoreThanProportion'
+    nom: number
+    denom: number
+}
+
+export interface V0BodyPart_Voice {
+    __kind: 'Voice'
+}
+
+export type V0BodyId = V0BodyId_Administration | V0BodyId_Defense | V0BodyId_Executive | V0BodyId_Index | V0BodyId_Judicial | V0BodyId_Legislative | V0BodyId_Named | V0BodyId_Technical | V0BodyId_Treasury | V0BodyId_Unit
+
+export interface V0BodyId_Administration {
+    __kind: 'Administration'
+}
+
+export interface V0BodyId_Defense {
+    __kind: 'Defense'
+}
+
+export interface V0BodyId_Executive {
+    __kind: 'Executive'
+}
+
+export interface V0BodyId_Index {
+    __kind: 'Index'
+    value: number
+}
+
+export interface V0BodyId_Judicial {
+    __kind: 'Judicial'
+}
+
+export interface V0BodyId_Legislative {
+    __kind: 'Legislative'
+}
+
+export interface V0BodyId_Named {
+    __kind: 'Named'
+    value: WeakBoundedVec
+}
+
+export interface V0BodyId_Technical {
+    __kind: 'Technical'
+}
+
+export interface V0BodyId_Treasury {
+    __kind: 'Treasury'
+}
+
+export interface V0BodyId_Unit {
+    __kind: 'Unit'
+}
+
+export type WeakBoundedVec = Bytes
+
+export type V0NetworkId = V0NetworkId_Any | V0NetworkId_Kusama | V0NetworkId_Named | V0NetworkId_Polkadot
+
+export interface V0NetworkId_Any {
+    __kind: 'Any'
+}
+
+export interface V0NetworkId_Kusama {
+    __kind: 'Kusama'
+}
+
+export interface V0NetworkId_Named {
+    __kind: 'Named'
+    value: WeakBoundedVec
+}
+
+export interface V0NetworkId_Polkadot {
+    __kind: 'Polkadot'
+}
+
+export interface V1MultiAsset {
+    id: V1AssetId
+    fun: V1Fungibility
+}
+
+export const AccountId32 = sts.bytes()
+
 export const V2Instruction: sts.Type<V2Instruction> = sts.closedEnum(() => {
     return  {
         BuyExecution: sts.enumStruct({
@@ -153,13 +448,13 @@ export const V2Error: sts.Type<V2Error> = sts.closedEnum(() => {
         FailedToTransactAsset: sts.unit(),
         InvalidLocation: sts.unit(),
         LocationCannotHold: sts.unit(),
+        MaxWeightInvalid: sts.unit(),
         MultiLocationFull: sts.unit(),
         MultiLocationNotInvertible: sts.unit(),
         NotHoldingFees: sts.unit(),
         NotWithdrawable: sts.unit(),
         Overflow: sts.unit(),
         TooExpensive: sts.unit(),
-        TooMuchWeightRequired: sts.unit(),
         Transport: sts.unit(),
         Trap: sts.bigint(),
         UnhandledXcmVersion: sts.unit(),
@@ -173,7 +468,7 @@ export const V2Error: sts.Type<V2Error> = sts.closedEnum(() => {
     }
 })
 
-export type V2Error = V2Error_AssetNotFound | V2Error_BadOrigin | V2Error_Barrier | V2Error_DestinationUnsupported | V2Error_ExceedsMaxMessageSize | V2Error_FailedToDecode | V2Error_FailedToTransactAsset | V2Error_InvalidLocation | V2Error_LocationCannotHold | V2Error_MultiLocationFull | V2Error_MultiLocationNotInvertible | V2Error_NotHoldingFees | V2Error_NotWithdrawable | V2Error_Overflow | V2Error_TooExpensive | V2Error_TooMuchWeightRequired | V2Error_Transport | V2Error_Trap | V2Error_UnhandledXcmVersion | V2Error_Unimplemented | V2Error_UnknownClaim | V2Error_Unroutable | V2Error_UntrustedReserveLocation | V2Error_UntrustedTeleportLocation | V2Error_WeightLimitReached | V2Error_WeightNotComputable
+export type V2Error = V2Error_AssetNotFound | V2Error_BadOrigin | V2Error_Barrier | V2Error_DestinationUnsupported | V2Error_ExceedsMaxMessageSize | V2Error_FailedToDecode | V2Error_FailedToTransactAsset | V2Error_InvalidLocation | V2Error_LocationCannotHold | V2Error_MaxWeightInvalid | V2Error_MultiLocationFull | V2Error_MultiLocationNotInvertible | V2Error_NotHoldingFees | V2Error_NotWithdrawable | V2Error_Overflow | V2Error_TooExpensive | V2Error_Transport | V2Error_Trap | V2Error_UnhandledXcmVersion | V2Error_Unimplemented | V2Error_UnknownClaim | V2Error_Unroutable | V2Error_UntrustedReserveLocation | V2Error_UntrustedTeleportLocation | V2Error_WeightLimitReached | V2Error_WeightNotComputable
 
 export interface V2Error_AssetNotFound {
     __kind: 'AssetNotFound'
@@ -211,6 +506,10 @@ export interface V2Error_LocationCannotHold {
     __kind: 'LocationCannotHold'
 }
 
+export interface V2Error_MaxWeightInvalid {
+    __kind: 'MaxWeightInvalid'
+}
+
 export interface V2Error_MultiLocationFull {
     __kind: 'MultiLocationFull'
 }
@@ -233,10 +532,6 @@ export interface V2Error_Overflow {
 
 export interface V2Error_TooExpensive {
     __kind: 'TooExpensive'
-}
-
-export interface V2Error_TooMuchWeightRequired {
-    __kind: 'TooMuchWeightRequired'
 }
 
 export interface V2Error_Transport {
@@ -302,252 +597,6 @@ export interface V2Response_Version {
     value: number
 }
 
-export interface V1MultiAsset {
-    id: V1AssetId
-    fun: V1Fungibility
-}
-
-export type V1Fungibility = V1Fungibility_Fungible | V1Fungibility_NonFungible
-
-export interface V1Fungibility_Fungible {
-    __kind: 'Fungible'
-    value: bigint
-}
-
-export interface V1Fungibility_NonFungible {
-    __kind: 'NonFungible'
-    value: V1AssetInstance
-}
-
-export type V1AssetInstance = V1AssetInstance_Array16 | V1AssetInstance_Array32 | V1AssetInstance_Array4 | V1AssetInstance_Array8 | V1AssetInstance_Blob | V1AssetInstance_Index | V1AssetInstance_Undefined
-
-export interface V1AssetInstance_Array16 {
-    __kind: 'Array16'
-    value: Bytes
-}
-
-export interface V1AssetInstance_Array32 {
-    __kind: 'Array32'
-    value: Bytes
-}
-
-export interface V1AssetInstance_Array4 {
-    __kind: 'Array4'
-    value: Bytes
-}
-
-export interface V1AssetInstance_Array8 {
-    __kind: 'Array8'
-    value: Bytes
-}
-
-export interface V1AssetInstance_Blob {
-    __kind: 'Blob'
-    value: Bytes
-}
-
-export interface V1AssetInstance_Index {
-    __kind: 'Index'
-    value: bigint
-}
-
-export interface V1AssetInstance_Undefined {
-    __kind: 'Undefined'
-}
-
-export type V1AssetId = V1AssetId_Abstract | V1AssetId_Concrete
-
-export interface V1AssetId_Abstract {
-    __kind: 'Abstract'
-    value: Bytes
-}
-
-export interface V1AssetId_Concrete {
-    __kind: 'Concrete'
-    value: V1MultiLocation
-}
-
-export interface V1MultiLocation {
-    parents: number
-    interior: V1Junctions
-}
-
-export type V1Junctions = V1Junctions_Here | V1Junctions_X1 | V1Junctions_X2 | V1Junctions_X3 | V1Junctions_X4 | V1Junctions_X5 | V1Junctions_X6 | V1Junctions_X7 | V1Junctions_X8
-
-export interface V1Junctions_Here {
-    __kind: 'Here'
-}
-
-export interface V1Junctions_X1 {
-    __kind: 'X1'
-    value: V1Junction
-}
-
-export interface V1Junctions_X2 {
-    __kind: 'X2'
-    value: [V1Junction, V1Junction]
-}
-
-export interface V1Junctions_X3 {
-    __kind: 'X3'
-    value: [V1Junction, V1Junction, V1Junction]
-}
-
-export interface V1Junctions_X4 {
-    __kind: 'X4'
-    value: [V1Junction, V1Junction, V1Junction, V1Junction]
-}
-
-export interface V1Junctions_X5 {
-    __kind: 'X5'
-    value: [V1Junction, V1Junction, V1Junction, V1Junction, V1Junction]
-}
-
-export interface V1Junctions_X6 {
-    __kind: 'X6'
-    value: [V1Junction, V1Junction, V1Junction, V1Junction, V1Junction, V1Junction]
-}
-
-export interface V1Junctions_X7 {
-    __kind: 'X7'
-    value: [V1Junction, V1Junction, V1Junction, V1Junction, V1Junction, V1Junction, V1Junction]
-}
-
-export interface V1Junctions_X8 {
-    __kind: 'X8'
-    value: [V1Junction, V1Junction, V1Junction, V1Junction, V1Junction, V1Junction, V1Junction, V1Junction]
-}
-
-export type V1Junction = V1Junction_AccountId32 | V1Junction_AccountIndex64 | V1Junction_AccountKey20 | V1Junction_GeneralIndex | V1Junction_GeneralKey | V1Junction_OnlyChild | V1Junction_PalletInstance | V1Junction_Parachain | V1Junction_Plurality
-
-export interface V1Junction_AccountId32 {
-    __kind: 'AccountId32'
-    network: V0NetworkId
-    id: Bytes
-}
-
-export interface V1Junction_AccountIndex64 {
-    __kind: 'AccountIndex64'
-    network: V0NetworkId
-    index: bigint
-}
-
-export interface V1Junction_AccountKey20 {
-    __kind: 'AccountKey20'
-    network: V0NetworkId
-    key: Bytes
-}
-
-export interface V1Junction_GeneralIndex {
-    __kind: 'GeneralIndex'
-    value: bigint
-}
-
-export interface V1Junction_GeneralKey {
-    __kind: 'GeneralKey'
-    value: Bytes
-}
-
-export interface V1Junction_OnlyChild {
-    __kind: 'OnlyChild'
-}
-
-export interface V1Junction_PalletInstance {
-    __kind: 'PalletInstance'
-    value: number
-}
-
-export interface V1Junction_Parachain {
-    __kind: 'Parachain'
-    value: number
-}
-
-export interface V1Junction_Plurality {
-    __kind: 'Plurality'
-    id: V0BodyId
-    part: V0BodyPart
-}
-
-export type V0BodyPart = V0BodyPart_AtLeastProportion | V0BodyPart_Fraction | V0BodyPart_Members | V0BodyPart_MoreThanProportion | V0BodyPart_Voice
-
-export interface V0BodyPart_AtLeastProportion {
-    __kind: 'AtLeastProportion'
-    nom: number
-    denom: number
-}
-
-export interface V0BodyPart_Fraction {
-    __kind: 'Fraction'
-    nom: number
-    denom: number
-}
-
-export interface V0BodyPart_Members {
-    __kind: 'Members'
-    count: number
-}
-
-export interface V0BodyPart_MoreThanProportion {
-    __kind: 'MoreThanProportion'
-    nom: number
-    denom: number
-}
-
-export interface V0BodyPart_Voice {
-    __kind: 'Voice'
-}
-
-export type V0BodyId = V0BodyId_Executive | V0BodyId_Index | V0BodyId_Judicial | V0BodyId_Legislative | V0BodyId_Named | V0BodyId_Technical | V0BodyId_Unit
-
-export interface V0BodyId_Executive {
-    __kind: 'Executive'
-}
-
-export interface V0BodyId_Index {
-    __kind: 'Index'
-    value: number
-}
-
-export interface V0BodyId_Judicial {
-    __kind: 'Judicial'
-}
-
-export interface V0BodyId_Legislative {
-    __kind: 'Legislative'
-}
-
-export interface V0BodyId_Named {
-    __kind: 'Named'
-    value: Bytes
-}
-
-export interface V0BodyId_Technical {
-    __kind: 'Technical'
-}
-
-export interface V0BodyId_Unit {
-    __kind: 'Unit'
-}
-
-export type V0NetworkId = V0NetworkId_Any | V0NetworkId_Kusama | V0NetworkId_Named | V0NetworkId_Polkadot
-
-export interface V0NetworkId_Any {
-    __kind: 'Any'
-}
-
-export interface V0NetworkId_Kusama {
-    __kind: 'Kusama'
-}
-
-export interface V0NetworkId_Named {
-    __kind: 'Named'
-    value: Bytes
-}
-
-export interface V0NetworkId_Polkadot {
-    __kind: 'Polkadot'
-}
-
 export const V1Junctions: sts.Type<V1Junctions> = sts.closedEnum(() => {
     return  {
         Here: sts.unit(),
@@ -577,7 +626,7 @@ export const V1Junction: sts.Type<V1Junction> = sts.closedEnum(() => {
             key: sts.bytes(),
         }),
         GeneralIndex: sts.bigint(),
-        GeneralKey: sts.bytes(),
+        GeneralKey: WeakBoundedVec,
         OnlyChild: sts.unit(),
         PalletInstance: sts.number(),
         Parachain: sts.number(),
@@ -611,21 +660,26 @@ export const V0BodyPart: sts.Type<V0BodyPart> = sts.closedEnum(() => {
 
 export const V0BodyId: sts.Type<V0BodyId> = sts.closedEnum(() => {
     return  {
+        Administration: sts.unit(),
+        Defense: sts.unit(),
         Executive: sts.unit(),
         Index: sts.number(),
         Judicial: sts.unit(),
         Legislative: sts.unit(),
-        Named: sts.bytes(),
+        Named: WeakBoundedVec,
         Technical: sts.unit(),
+        Treasury: sts.unit(),
         Unit: sts.unit(),
     }
 })
+
+export const WeakBoundedVec = sts.bytes()
 
 export const V0NetworkId: sts.Type<V0NetworkId> = sts.closedEnum(() => {
     return  {
         Any: sts.unit(),
         Kusama: sts.unit(),
-        Named: sts.bytes(),
+        Named: WeakBoundedVec,
         Polkadot: sts.unit(),
     }
 })
@@ -663,13 +717,6 @@ export interface V1WildFungibility_Fungible {
 export interface V1WildFungibility_NonFungible {
     __kind: 'NonFungible'
 }
-
-export const V1AssetId: sts.Type<V1AssetId> = sts.closedEnum(() => {
-    return  {
-        Abstract: sts.bytes(),
-        Concrete: V1MultiLocation,
-    }
-})
 
 export type V1WildMultiAsset = V1WildMultiAsset_All | V1WildMultiAsset_AllOf
 
@@ -712,32 +759,6 @@ export interface V2WeightLimit_Limited {
 export interface V2WeightLimit_Unlimited {
     __kind: 'Unlimited'
 }
-
-export const V1MultiAsset: sts.Type<V1MultiAsset> = sts.struct(() => {
-    return  {
-        id: V1AssetId,
-        fun: V1Fungibility,
-    }
-})
-
-export const V1Fungibility: sts.Type<V1Fungibility> = sts.closedEnum(() => {
-    return  {
-        Fungible: sts.bigint(),
-        NonFungible: V1AssetInstance,
-    }
-})
-
-export const V1AssetInstance: sts.Type<V1AssetInstance> = sts.closedEnum(() => {
-    return  {
-        Array16: sts.bytes(),
-        Array32: sts.bytes(),
-        Array4: sts.bytes(),
-        Array8: sts.bytes(),
-        Blob: sts.bytes(),
-        Index: sts.bigint(),
-        Undefined: sts.unit(),
-    }
-})
 
 export type V2Instruction = V2Instruction_BuyExecution | V2Instruction_ClaimAsset | V2Instruction_ClearError | V2Instruction_ClearOrigin | V2Instruction_DepositAsset | V2Instruction_DepositReserveAsset | V2Instruction_DescendOrigin | V2Instruction_ExchangeAsset | V2Instruction_HrmpChannelAccepted | V2Instruction_HrmpChannelClosing | V2Instruction_HrmpNewChannelOpenRequest | V2Instruction_InitiateReserveWithdraw | V2Instruction_InitiateTeleport | V2Instruction_QueryHolding | V2Instruction_QueryResponse | V2Instruction_ReceiveTeleportedAsset | V2Instruction_RefundSurplus | V2Instruction_ReportError | V2Instruction_ReserveAssetDeposited | V2Instruction_SetAppendix | V2Instruction_SetErrorHandler | V2Instruction_SubscribeVersion | V2Instruction_Transact | V2Instruction_TransferAsset | V2Instruction_TransferReserveAsset | V2Instruction_Trap | V2Instruction_UnsubscribeVersion | V2Instruction_WithdrawAsset
 
@@ -912,5 +933,3 @@ export const V1MultiLocation: sts.Type<V1MultiLocation> = sts.struct(() => {
         interior: V1Junctions,
     }
 })
-
-export const AccountId32 = sts.bytes()
