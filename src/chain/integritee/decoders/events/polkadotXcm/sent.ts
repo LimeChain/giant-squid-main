f9101fe9ce140dd1062630413e3a4697a1f1facb
nikolay_cholakov <nikolaicholakov25@gmail.com>
2025-06-05 15:11:10 +0300

    refactor: use new xToken decoder on integritee

--- src/chain/integritee/decoders/events/polkadotXcm/sent.ts
+++ src/chain/integritee/decoders/events/polkadotXcm/sent.ts
@@ -3,9 +3,24 @@ import { UnknownVersionError } from '@/utils';
 import { Event, ISentEventPalletDecoder } from '@/indexer';
 
 import assert from 'assert';
-import { V1MultiLocation, V2Instruction, V2Instruction_BuyExecution } from '@/chain/integritee/types/integriteeParachainV14';
-import { V3MultiLocation, V3Instruction, V3Instruction_BuyExecution } from '@/chain/integritee/types/integriteeParachainV35';
-import { V4Location, V4Instruction, V4Instruction_BuyExecution } from '@/chain/integritee/types/integriteeParachainV520';
+import { V2Instruction_BuyExecution } from '@/chain/integritee/types/integriteeParachainV14';
+import { V3Instruction_BuyExecution } from '@/chain/integritee/types/integriteeParachainV35';
+import { V4Instruction_BuyExecution } from '@/chain/integritee/types/integriteeParachainV520';
+
+import {
+  getAssetAmount,
+  getRawAssetFromInstruction,
+  getTransferTarget,
+  getTransferTargetV4,
+  getOriginCaller,
+  getDestination,
+  getWeightLimit,
+  getWeightLimitV3V4,
+  getDestinationV4,
+  getOriginCallerV4,
+  getAssetAmountV4,
+  getRawAssetFromInstructionV4,
+} from '@/indexer/pallets/polkadot-xcm/events/sent';
 
 export class SentEventPalletDecoder implements ISentEventPalletDecoder {
   decode(event: Event) {
@@ -20,12 +35,19 @@ export class SentEventPalletDecoder implements ISentEventPalletDecoder {
       if (!from) return;
 
       const weightLimitMsg = message.find((msg) => msg.__kind === 'BuyExecution') as V2Instruction_BuyExecution;
+      const transferTarget = getTransferTarget(message.at(-1)!, from.value);
+      const assetAmount = getAssetAmount(message[0]);
+      const rawAssets = getRawAssetFromInstruction(message[0]);
+
       return {
         from,
         toChain: getDestination(destination),
-        amount: getAmount(message[0]),
-        to: getTarget(message.at(-1)!, from),
+        amount: assetAmount,
+        to: transferTarget,
         weightLimit: getWeightLimit(weightLimitMsg),
+        asset: rawAssets,
+        contractCalled: event?.call?.args?.transaction?.value?.action?.value,
+        contractInput: event?.call?.args?.transaction?.value?.input,
       };
     } else if (sent.integriteeParachainV35.is(event)) {
       const [origin, destination, message] = sent.integriteeParachainV35.decode(event);
@@ -34,13 +56,20 @@ export class SentEventPalletDecoder implements ISentEventPalletDecoder {
       const from = getOriginCaller(origin);
       if (!from) return;
 
-      const weightLimitMsg = message.find((msg) => msg.__kind === 'BuyExecution') as V2Instruction_BuyExecution;
+      const weightLimitMsg = message.find((msg) => msg.__kind === 'BuyExecution') as V3Instruction_BuyExecution;
+      const transferTarget = getTransferTarget(message.at(-1)!, from.value);
+      const assetAmount = getAssetAmount(message[0]);
+      const rawAssets = getRawAssetFromInstruction(message[0]);
+
       return {
         from,
         toChain: getDestination(destination),
-        amount: getAmount(message[0]),
-        to: getTarget(message.at(-1)!, from),
-        weightLimit: getWeightLimit(weightLimitMsg),
+        amount: assetAmount,
+        to: transferTarget,
+        weightLimit: getWeightLimitV3V4(weightLimitMsg),
+        asset: rawAssets,
+        contractCalled: event?.call?.args?.transaction?.value?.action?.value,
+        contractInput: event?.call?.args?.transaction?.value?.input,
       };
     } else if (sent.integriteeParachainV42.is(event)) {
       const { origin, destination, message, messageId } = sent.integriteeParachainV42.decode(event);
@@ -50,12 +79,19 @@ export class SentEventPalletDecoder implements ISentEventPalletDecoder {
       if (!from) return;
 
       const weightLimitMsg = message.find((msg) => msg.__kind === 'BuyExecution') as V3Instruction_BuyExecution;
+      const transferTarget = getTransferTarget(message.at(-1)!, from.value);
+      const assetAmount = getAssetAmount(message[0]);
+      const rawAssets = getRawAssetFromInstruction(message[0]);
+
       return {
         from,
         toChain: getDestination(destination),
-        amount: getAmount(message[0]),
-        to: getTarget(message.at(-1)!, from),
+        amount: assetAmount,
+        to: transferTarget,
         weightLimit: getWeightLimitV3V4(weightLimitMsg),
+        asset: rawAssets,
+        contractCalled: event?.call?.args?.transaction?.value?.action?.value,
+        contractInput: event?.call?.args?.transaction?.value?.input,
       };
     } else if (sent.integriteeParachainV520.is(event)) {
       const { origin, destination, message, messageId } = sent.integriteeParachainV520.decode(event);
@@ -65,151 +101,22 @@ export class SentEventPalletDecoder implements ISentEventPalletDecoder {
       if (!from) return;
 
       const weightLimitMsg = message.find((msg) => msg.__kind === 'BuyExecution') as V4Instruction_BuyExecution;
+      const transferTarget = getTransferTargetV4(message.at(-1)!, from.value);
+      const assetAmount = getAssetAmountV4(message[0]);
+      const rawAssets = getRawAssetFromInstructionV4(message[0]);
+
       return {
         from,
         toChain: getDestinationV4(destination),
-        amount: getAmount(message[0]),
-        to: getTargetV4(message.at(-1)!, from),
+        amount: assetAmount,
+        to: transferTarget,
         weightLimit: getWeightLimitV3V4(weightLimitMsg),
+        asset: rawAssets,
+        contractCalled: event?.call?.args?.transaction?.value?.action?.value,
+        contractInput: event?.call?.args?.transaction?.value?.input,
       };
     }
 
     throw new UnknownVersionError(sent);
   }
-}
-
-function getOriginCaller(origin: V1MultiLocation | V3MultiLocation) {
-  if (origin.interior.__kind === 'X1') {
-    switch (origin.interior.value.__kind) {
-      case 'AccountId32':
-        return origin.interior.value.id;
-      case 'AccountKey20':
-        return origin.interior.value.key;
-    }
-  }
-
-  return;
-}
-
-function getOriginCallerV4(origin: V4Location) {
-  if (origin.interior.__kind === 'X1') {
-    switch (origin.interior.value[0].__kind) {
-      case 'AccountId32':
-        return origin.interior.value[0].id;
-      case 'AccountKey20':
-        return origin.interior.value[0].key;
-    }
-  }
-
-  return;
-}
-
-function getDestination(destination: V1MultiLocation | V3MultiLocation) {
-  if (destination.interior.__kind === 'X1') {
-    switch (destination.interior.value.__kind) {
-      case 'Parachain':
-        return destination.interior.value.value.toString();
-      case 'AccountId32':
-        return destination.interior.value.id;
-      case 'AccountKey20':
-        return destination.interior.value.key;
-    }
-
-    return;
-  }
-  // Destination is the relay chain
-  else if (destination.interior.__kind === 'Here') {
-    return destination.interior.__kind;
-  }
-
-  return;
-}
-
-function getDestinationV4(destination: V4Location) {
-  if (destination.interior.__kind === 'X1') {
-    switch (destination.interior.value[0].__kind) {
-      case 'Parachain':
-        return destination.interior.value[0].value.toString();
-      case 'AccountId32':
-        return destination.interior.value[0].id;
-      case 'AccountKey20':
-        return destination.interior.value[0].key;
-    }
-
-    return;
-  }
-  // Destination is the relay chain
-  else if (destination.interior.__kind === 'Here') {
-    return destination.interior.__kind;
-  }
-
-  return;
-}
-
-function getAmount(message: V2Instruction | V3Instruction | V4Instruction) {
-  switch (message.__kind) {
-    case 'WithdrawAsset':
-    case 'ReserveAssetDeposited':
-      return message.value[0].fun.__kind === 'Fungible' ? message.value[0].fun.value : undefined;
-    case 'TransferReserveAsset':
-      return message.assets[0].fun.__kind === 'Fungible' ? message.assets[0].fun.value : undefined;
-    default:
-      return;
-  }
-}
-
-function getTarget(message: V2Instruction | V3Instruction, from?: string) {
-  // Call are to other parachains
-  if (message.__kind === 'DepositAsset') {
-    if (message.beneficiary.interior.__kind === 'X1') {
-      switch (message.beneficiary.interior.value.__kind) {
-        case 'AccountId32':
-          return message.beneficiary.interior.value.id;
-        case 'AccountKey20':
-          return message.beneficiary.interior.value.key;
-        default:
-          return;
-      }
-    }
-  }
-
-  // Calls are to assetHub
-  if (message?.__kind !== 'DepositAsset') {
-    return from;
-  }
-
-  return;
-}
-
-function getTargetV4(message: V4Instruction, from?: string) {
-  // Call are to other parachains
-  if (message.__kind === 'DepositAsset') {
-    if (message.beneficiary.interior.__kind === 'X1') {
-      switch (message.beneficiary.interior.value[0].__kind) {
-        case 'AccountId32':
-          return message.beneficiary.interior.value[0].id;
-        case 'AccountKey20':
-          return message.beneficiary.interior.value[0].key;
-        default:
-          return;
-      }
-    }
-  }
-
-  // Calls are to assetHub
-  if (message?.__kind !== 'DepositAsset') {
-    return from;
-  }
-
-  return;
-}
-
-function getWeightLimit(message: V2Instruction_BuyExecution | undefined) {
-  if (message?.weightLimit?.__kind === 'Limited') return message.weightLimit.value;
-  else return;
-}
-
-function getWeightLimitV3V4(message: V3Instruction_BuyExecution | V4Instruction_BuyExecution | undefined) {
-  if (message?.weightLimit?.__kind === 'Limited') return message.weightLimit.value.proofSize;
-  else return;
 }
